d4546300f8c6c4ce3acdabbf3da3515ab17d9566
Geraska <tuftedfun@gmail.com>
2022-12-22 16:36:20 +0600

    learnings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fuck you!");
+        Person tom = new Person();
+        tom.displayInfo();
+
+        Person bob = new Person("Bob");
+        bob.displayInfo();
+
+        Person sam = new Person("Sam", 32);
+        sam.displayInfo();
+        Collection<String> collectionA = Arrays.asList("S", "T");
+        Collection<String> collectionB = Arrays.asList("U", "V");
+        System.out.println(collectionA.addAll(collectionB));
+    }
+}
+
+class Person {
+    String name;
+    int age;
+    {
+        name = "Undefined";
+        age = 18;
+    }
+    Person()
+    {
+    }
+    Person(String name)
+    {
+        this(name, 18);
+    }
+    Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    void displayInfo(){
+        System.out.printf("Name %s \tAge %d\n", name, age);
     }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,109 @@
 import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input name: ");
-        String name = in.nextLine();
-        System.out.print("Input age: ");
-        byte age = in.nextByte();
-        System.out.print("Input height: ");
-        float height = in.nextFloat();
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter your name: ");
+//        String name = scan.nextLine();
+//        System.out.print("Enter your age: ");
+//        int age =  scan.nextInt();
+//        System.out.print("Enter your height: ");
+//        float height =  scan.nextFloat();
+//        display(name, age, height);
+//        System.out.println(fibonachi(8));
+        try{
+            int[] numbers = new int[3];
+            numbers[2]=45;
+            System.out.println(numbers[2]);
+        }
+        catch(Exception ex){
 
-        System.out.printf("Name %s Age %d Height %.2f \n", name, age, height);
+            ex.printStackTrace();
+        }
+        finally{
+            System.out.println("Блок finally");
+        }
+        System.out.println("Программа завершена");
+    }
+    static int fibonachi(int n){
+
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        else{
+            return fibonachi(n - 1) + fibonachi(n - 2);
+        }
+    }
+    static int sums(int a, int b, int c) {
+        return a + b + c;
+    }
+//    static void sum(String msg, int ...num){
+//        System.out.println(msg);
+//        int result = 0;
+//        for (int n:num) {
+//            result += n;
+//            System.out.println(result);
+//        }
+//    }
+
+    static void scanner() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input num1: ");
+        byte num1 = in.nextByte();
+        System.out.print("Input num2: ");
+        byte num2 = in.nextByte();
+        int num = num1<num2? (num1+num2) : (num1-num2);
+        System.out.printf("%d", num);
         in.close();
+        System.out.println();
+    }
+
+    static void display(String name, int age, float height){
+        System.out.printf("Name %s Age %d Height %.2f", name, age, height);
+        System.out.println();
+    }
+
+    static void arrays() {
+//        int[] nums = new int[4];
+//        nums[0] = 1;
+//        nums[1] = 2;
+//        nums[2] = 3;
+//        nums[3] = 4;
+//        System.out.println(nums.length - 1);
+//        int[][] nums1 = new int[2][3];
+//        nums1[1][0] = 5;
+//        System.out.println(nums1);
+        int[][] nums = new int[][]
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                };
+        for (int i = 0; i < nums.length; i++){
+            for(int j=0; j < nums[i].length; j++){
+
+                System.out.printf("%d ", nums[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void dataTypes() {
+//        boolean b = 1 > 4;
+//        boolean b1 = 1 != 4;
+//        boolean b2 = 1 < 4;
+//        System.out.println(b);
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        int a = 4;
+//        byte b = (byte) a;
+//        System.out.println(b);
+//        int n = 10;
+//        for(int i=0, j = n - 1; i < j; i++, j--){
+//
+//            System.out.println(i * j);
+//        }
     }
 }
